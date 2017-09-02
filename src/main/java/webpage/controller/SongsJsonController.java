5b3a0158c68fec557fb2b63798e40ac85389441f
Mateo Murillo Penagos <mamup111@hotmail.com>
2017-09-02 11:37:58 -0500

    Prueba DB 1

--- src/main/java/webpage/controller/SongsJsonController.java
+++ src/main/java/webpage/controller/SongsJsonController.java
@@ -1,11 +1,15 @@
 package webpage.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import webpage.model.SongsRepository;
 import webpage.model.UserRepository;
 import webpage.views.entities.Song;
+import webpage.views.entities.User;
 
 @Controller
 public class SongsJsonController {
@@ -22,29 +27,36 @@ public class SongsJsonController {
 	@Autowired
 	UserRepository userRepo;
 
-	
 	@RequestMapping(value = "/findAllSong", method = RequestMethod.GET)
 	public ResponseEntity<?> findAll() {
 		return new ResponseEntity<>(songRepo.findAll(), HttpStatus.OK);
 	}
+	@RequestMapping(value = "/findAllUser", method = RequestMethod.GET)
+	public ResponseEntity<?> findAlluser() {
+		return new ResponseEntity<>(userRepo.findAll(), HttpStatus.OK);
+	}
 
-	@RequestMapping(value = "/songs", method = RequestMethod.POST)
+	@RequestMapping(value = "/songstest", method = RequestMethod.POST)
 	public ResponseEntity<?> saveSong(@RequestBody List<Song> songs) {
 		songRepo.save(songs);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-//
-//	@RequestMapping(value = "/song/{cod}", method = RequestMethod.DELETE)
-//	public ResponseEntity<?> deleteSong(@PathVariable("cod") long cod) {
-//		songRepo.delete(cod);
-//		return new ResponseEntity<>(HttpStatus.OK);
-//	}
-//
-//	@RequestMapping(value = "/song/title", method = RequestMethod.GET)
-//	public ResponseEntity<?> findByName(@RequestParam("title") String title) {
-//		return new ResponseEntity<>(songRepo.findByTitleIgnoreCaseContaining(title), HttpStatus.OK);
-//	}
-//
+
 	
-	
+	//
+	// @RequestMapping(value = "/song/{cod}", method = RequestMethod.DELETE)
+	// public ResponseEntity<?> deleteSong(@PathVariable("cod") long cod) {
+	// songRepo.delete(cod);
+	// return new ResponseEntity<>(HttpStatus.OK);
+	// }
+	//
+	// @RequestMapping(value = "/song/title", method = RequestMethod.GET)
+	// public ResponseEntity<?> findByName(@RequestParam("title") String title)
+	// {
+	// return new
+	// ResponseEntity<>(songRepo.findByTitleIgnoreCaseContaining(title),
+	// HttpStatus.OK);
+	// }
+	//
+
 }
--- src/main/java/webpage/views/entities/User.java
+++ src/main/java/webpage/views/entities/User.java
@@ -29,7 +29,25 @@ public class User {
 	@Email
 	private String email;
 
+	protected User(){}
 
+	public User(int i){
+		switch (i){
+		case 1:
+			password="12345678";
+			break;
+		case 2:
+			username="Estaaaaa";
+			password="12345678";
+			break;
+		case 3:
+			username="Lorraaaaaa";
+			password="12345678";
+			break;
+		}
+		
+	}
+	
 	/**
 	 * @return the cod
 	 */
