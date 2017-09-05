e65c280682046295368abe44db301d6a964ba14e
Mateo Murillo Penagos <mamup111@hotmail.com>
2017-09-05 15:33:01 -0500

    Another test db failover

--- src/main/java/webpage/views/ContextConfiguration.java
+++ src/main/java/webpage/views/ContextConfiguration.java
@@ -11,34 +11,34 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 class ContextConfiguration {
-
-	@Bean(name="principal")
-	@Primary
-	@ConfigurationProperties(prefix="datasource.principal")
-	public DataSource primaryDataSource() {
-//		final HikariDataSource ds = new HikariDataSource();
-//		ds.setMaximumPoolSize(10);
-//		ds.setDriverClassName("org.postgresql.Driver");
-//		ds.setJdbcUrl("jdbc:postgresql://10.131.137.187/music");
-//		ds.setUsername("postgres");
-//		ds.setPassword("eafit.2016");
-//		ds.setConnectionTimeout(20000);
-//		return ds;
-		return DataSourceBuilder.create().build();
-	}	
-	
-	@Bean(name="secondary")
-	@ConfigurationProperties(prefix="datasource.secondary")
-	public DataSource secondaryDataSource() {
-//		final HikariDataSource ds = new HikariDataSource();
-//		ds.setMaximumPoolSize(10);
-//		ds.setDriverClassName("org.postgresql.Driver");
-//		ds.setJdbcUrl("jdbc:postgresql://10.131.137.218/music");
-//		ds.setUsername("postgres");
-//		ds.setPassword("eafit.2016");
-//		ds.setConnectionTimeout(20000);
-//		return ds;
-		return DataSourceBuilder.create().build();
-		
-	}	
+//
+//	@Bean(name="principal")
+//	@Primary
+//	@ConfigurationProperties(prefix="datasource.principal")
+//	public DataSource primaryDataSource() {
+////		final HikariDataSource ds = new HikariDataSource();
+////		ds.setMaximumPoolSize(10);
+////		ds.setDriverClassName("org.postgresql.Driver");
+////		ds.setJdbcUrl("jdbc:postgresql://10.131.137.187/music");
+////		ds.setUsername("postgres");
+////		ds.setPassword("eafit.2016");
+////		ds.setConnectionTimeout(20000);
+////		return ds;
+//		return DataSourceBuilder.create().build();
+//	}	
+//	
+//	@Bean(name="secondary")
+//	@ConfigurationProperties(prefix="datasource.secondary")
+//	public DataSource secondaryDataSource() {
+////		final HikariDataSource ds = new HikariDataSource();
+////		ds.setMaximumPoolSize(10);
+////		ds.setDriverClassName("org.postgresql.Driver");
+////		ds.setJdbcUrl("jdbc:postgresql://10.131.137.218/music");
+////		ds.setUsername("postgres");
+////		ds.setPassword("eafit.2016");
+////		ds.setConnectionTimeout(20000);
+////		return ds;
+//		return DataSourceBuilder.create().build();
+//		
+//	}	
 }
